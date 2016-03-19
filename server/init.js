f64b89a5b674260a5fc7de6f1ee631efddf77f5b
Kai Mysliwiec <kai.mysliwiec@prosiebensat1digital.de>
2016-03-19 19:34:18 +0100

    connecting twitter to socket.io

--- server/init.js
+++ server/init.js
@@ -4,9 +4,9 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var socketIO = require('socket.io');
+var streamer = require('./streamer');
 
-var testStreams = require('../streams');
-
+// express setup
 var app = express();
 app.use(function(req, res, next) {
   console.log('%s %s', req.method, req.url, req.headers['user-agent']);
@@ -15,33 +15,32 @@ app.use(function(req, res, next) {
 app.use(express.static(path.join(__dirname, '../public')));
 var server = app.listen(8888);
 
+// socket.io setup
 var io = socketIO(server);
-
 io.on('connection', function(socket) {
   console.log('a user connected');
-  var interval = setInterval(function() {
-    var stream = testStreams[Math.round((testStreams.length - 1) * Math.random())];
-    stream.watcher = Math.round(100 * Math.random());
-    var data = {
-      id: stream.id,
-      created_at: stream.created_at,
-      user_name: stream.user_name,
-      avatar: stream.avatar,
-      location: stream.location,
-      text: stream.text,
-      stream: 'http://server.com',
-      screenShot: 'http://url.to.screenshot',
-      watcher: stream.watcher
-    };
-    socket.emit('newStream', data);
-    console.log('message emmited');
-  }, 1000);
   socket.on('disconnect', function() {
-    clearInterval(interval);
     console.log('user disconnected');
   });
 });
 
 server.listen(8888, function(){
   console.log('listening on *:8888');
+});
+
+// twitter feed setup
+streamer.start('', function(stream) {
+  var data = {
+    id: stream.id,
+    created_at: stream.created_at,
+    user_name: stream.user_name,
+    avatar: stream.avatar,
+    location: stream.location,
+    text: stream.text,
+    stream: 'http://server.com',
+    screenShot: 'http://url.to.screenshot',
+    watcher: stream.watcher
+  };
+  io.emit('newStream', data);
+  console.log('message emmited');
 });
